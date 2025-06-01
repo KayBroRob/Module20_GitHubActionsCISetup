857ca90c70257a33f5f312bdc335f001c319855f
KayBroRob <kaylynbrooke.r@gmail.com>
2025-06-01 15:38:03 -0700

    updated for render deployment again

--- start/server/src/seeds/cleanDb.ts
+++ start/server/src/seeds/cleanDb.ts
@@ -3,7 +3,13 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db!.db.listCollections({
+    const model = models[modelName];
+    if (!model || !model.db){
+      console.warn('Model is undefined for model: ${modelName}');
+      return;
+    }
+
+    let modelExists = await model.db.db.listCollections({
       name: collectionName
     }).toArray()
 
